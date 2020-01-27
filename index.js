fcd833f02f8533439e25b05314b88fb56bef2052
mohammad saied <mohammadsolingen@gmail.com>
2020-01-27 16:14:12 +0100

    mohammad

---
+++ index.js
@@ -0,0 +1,34 @@
+class myClass {
+    constructor(button,classname,ul){
+        this.button = document.querySelector(button)
+        this.userName = document.querySelector(classname)
+        this.ul = document.querySelector(ul)
+        this.button.addEventListener("click",this.myData.bind(this))
+       
+    }
+
+    async  myData(){
+        if(this.userName.value=="")return
+        this.ul.innerHTML=""
+     await fetch(`https://api.github.com/users/${this.userName.value}/repos`)
+       .then((response) => {
+         return response.json();
+       })
+       .then((myJson) => {
+        myJson.forEach(element => {
+        var myList=  `
+            <li class=" list-group-item"><div class=" d-flex justify-content-between"> <span>${element.full_name}</span><span> ${(element.created_at).slice(8,10)}-${(element.created_at).slice(5,7)}-${(element.created_at).slice(0,4)}</span></div>
+            <span class="d-block">${element.description}</span>
+            <span class="d-block"><a href="${element.html_url}">${element.html_url}</a></span>
+            </li> 
+        `
+ this.ul.insertAdjacentHTML("afterbegin",myList)
+
+});
+
+       });
+       this.userName.value=""
+     }
+}
+var getData = new myClass("button",".form-control","ul")
+
