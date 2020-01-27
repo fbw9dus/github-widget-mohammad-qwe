7577cdfb51b5a68c722029f4fd9aad000ff5646d
mohammad saied <mohammadsolingen@gmail.com>
2020-01-27 16:26:08 +0100

    mohammad

--- index.js
+++ index.js
@@ -10,21 +10,21 @@ class myClass {
     async  myData(){
         if(this.userName.value=="")return
         this.ul.innerHTML=""
-     await fetch(`https://api.github.com/users/${this.userName.value}/repos`)
-       .then((response) => {
-         return response.json();
-       })
-       .then((myJson) => {
+    let response = await fetch(`https://api.github.com/users/${this.userName.value}/repos`)
+       
+         let myJson= await  response.json()
+      
+       
         myJson.forEach(element => {
         var myList=  `
-            <li class=" list-group-item"><div class=" d-flex justify-content-between"> <span>${element.full_name}</span><span> ${(element.created_at).slice(8,10)}-${(element.created_at).slice(5,7)}-${(element.created_at).slice(0,4)}</span></div>
+            <li class=" list-group-item"><div class=" d-flex justify-content-between"> <span>${element.full_name}</span><span> ${(element.created_at).slice(8,10)}.${(element.created_at).slice(5,7)}.${(element.created_at).slice(0,4)}</span></div>
             <span class="d-block">${element.description}</span>
             <span class="d-block"><a href="${element.html_url}">${element.html_url}</a></span>
             </li> 
         `
  this.ul.insertAdjacentHTML("afterbegin",myList)
 
-});
+
 
        });
        this.userName.value=""
